4311c7ac073b2ac8eaeed6839a38fb2af7508a0b
Max Spliethöver <m.spliethoever@gmail.com>
2017-08-15 14:53:09 +0200

    Remove unused comments

--- test/unit/saw-link-element.spec.js
+++ test/unit/saw-link-element.spec.js
@@ -17,35 +17,9 @@ describe("saw-link-element", () => {
     expect(SawLinkElementComponent.props.linkItem.required).toBeTruthy();
   });
 
-  // Inspect the raw component options
-  // it("has a created hook", () => {
-  //   expect(typeof SawDeleteLinkComponent.created).toBe("function");
-  // });
-
   // Inspect imported the correct components
   it("has a created a delete function", () => {
     expect(SawLinkElementComponent.components["saw-delete-link"])
       .toBeTruthy();
   });
-
-  // Evaluate the results of functions in the raw component options
-  // it("sets the correct default data", () => {
-  //   expect(typeof SawDeleteLinkComponent.data).toBe("function");
-
-  //   const defaultData = SawDeleteLinkComponent.data();
-  //   expect(defaultData.message).toBe("hello!");
-  // });
-
-  // Inspect the component instance on mount
-  // it("correctly sets the message when created", () => {
-  //   const vm = new Vue(SawDeleteLinkComponent).$mount();
-  //   expect(vm.message).toBe("bye!");
-  // });
-
-  // Mount an instance and inspect the render output
-  // it("renders the correct message", () => {
-  //   const Ctor = Vue.extend(SawDeleteLinkComponent);
-  //   const vm = new Ctor().$mount();
-  //   expect(vm.$el.textContent).toBe("bye!");
-  // });
 });
